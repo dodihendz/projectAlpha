fded8dcd73c00ad7bf87ca353f4d7f7c04d89104
duyduyomm <dodiforgame@gmail.com>
2025-10-02 21:06:40 +0700

    feat viewes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+{{-- @dd($posts)  sama seperti var_dum --}}
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BmEndess | Blog Page</title>
-</head>
-
-<body>
-    <h1>This is my Blog Page</h1>
-</body>
-
-</html>
+@section('container')
+@foreach ($posts as $pst)
+<article class=" mb-5 border-bottom pb-3">
+    <h2>
+        <a href="/posts/{{$pst['slug']}}">Judul {{ $pst["title"] }}</a>
+    </h2>
+    <h5>By. {{ $pst["author"] }}</h5>
+    <p>{{ $pst["body"] }}</p>
+</article>
+@endforeach
+@endsection
