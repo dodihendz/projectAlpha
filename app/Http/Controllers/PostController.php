a81914d20ab0ad8067f6850125aff010b4224930
duyduyomm <dodiforgame@gmail.com>
2025-10-06 22:40:18 +0700

    feat postmodel view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,11 +12,11 @@ class PostController extends Controller
         return view('posts', ["title" => "Blog", "posts" => Post::all()]);
     }
 
-    public function show($slug)
+    public function show(Post $post)
     {
         return view('post', [
             "title" => "Single Post",
-            "post" => Post::find($slug)
+            "post" => $post
         ]);
     }
 }
