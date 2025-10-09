15be9a9d980f777d62e0c31863f63f5d632f67de
duyduyomm <dodiforgame@gmail.com>
2025-10-09 18:29:15 +0700

    feat Fac & faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,24 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(5)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            "name" => "My First User",
-            "email" => "youmain@gmail.com",
-            "password" => bcrypt("123456")
-        ]);
+        // User::create([
+        //     "name" => "My First User",
+        //     "email" => "youmain@gmail.com",
+        //     "password" => bcrypt("123456")
+        // ]);
 
-        User::create([
-            "name" => "My second User",
-            "email" => "youUser2@gmail.com",
-            "password" => bcrypt("123456")
-        ]);
+        // User::create([
+        //     "name" => "My second User",
+        //     "email" => "youUser2@gmail.com",
+        //     "password" => bcrypt("123456")
+        // ]);
 
         Category::create([
             "name" => "web-programming",
@@ -49,22 +49,24 @@ class DatabaseSeeder extends Seeder
             "slug" => "draw-03"
         ]);
 
-        post::create([
-            "category_id" => 1,
-            "user_id" => 2,
-            "title" => "Judul Pertama",
-            "slug" => "judul-pertama1",
-            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem, quod, quia voluptas quos voluptates quibusdam voluptatem voluptatibus quas voluptatum",
-            "body" => "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, nostrum iusto? Cupiditate ipsa ducimus reprehenderit sunt itaque, quod maxime fugit quidem quasi beatae optio exercitationem rerum, ex aliquam cumque velit</p>"
-        ]);
+        Post::factory(20)->create();
 
-        post::create([
-            "title" => "Judul Kedua",
-            "category_id" => 2,
-            "user_id" => 1,
-            "slug" => "judul-kedua2",
-            "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
-            "body" => "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, nostrum iusto? Cupiditate ipsa ducimus reprehenderit sunt itaque, quod maxime fugit quidem quasi beatae optio exercitationem rerum, ex aliquam cumque velit<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, eligendi, iste provident minus deserunt maxime omnis numquam non autem officiis similique repudiandae! Sint velit veniam vero illo, nobis repudiandae magni.</p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque blanditiis ducimus, dolorum, nesciunt quod soluta adipisci nobis ut quia optio error laboriosam rem eum sed quidem aspernatur dolores consequuntur dicta</p>"
-        ]);
+        // post::create([
+        //     "category_id" => 1,
+        //     "user_id" => 2,
+        //     "title" => "Judul Pertama",
+        //     "slug" => "judul-pertama1",
+        //     "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem, quod, quia voluptas quos voluptates quibusdam voluptatem voluptatibus quas voluptatum",
+        //     "body" => "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, nostrum iusto? Cupiditate ipsa ducimus reprehenderit sunt itaque, quod maxime fugit quidem quasi beatae optio exercitationem rerum, ex aliquam cumque velit</p>"
+        // ]);
+
+        // post::create([
+        //     "title" => "Judul Kedua",
+        //     "category_id" => 2,
+        //     "user_id" => 1,
+        //     "slug" => "judul-kedua2",
+        //     "excerpt" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
+        //     "body" => "<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, nostrum iusto? Cupiditate ipsa ducimus reprehenderit sunt itaque, quod maxime fugit quidem quasi beatae optio exercitationem rerum, ex aliquam cumque velit<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, eligendi, iste provident minus deserunt maxime omnis numquam non autem officiis similique repudiandae! Sint velit veniam vero illo, nobis repudiandae magni.</p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque blanditiis ducimus, dolorum, nesciunt quod soluta adipisci nobis ut quia optio error laboriosam rem eum sed quidem aspernatur dolores consequuntur dicta</p>"
+        // ]);
     }
 }
